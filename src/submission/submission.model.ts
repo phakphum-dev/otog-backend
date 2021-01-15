73f30a6d4972d4a7521ad6085aaa76216d06696f
Karnjj <karnjj@hotmail.com>
2021-01-15 16:06:48 +0700

    ✨add submission route

---
+++ src/submission/submission.model.ts
@@ -0,0 +1,38 @@
+import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
+
+function strToObj(data:string) {
+  return (data == null) ? [] : JSON.parse(data)
+}
+
+@Table({tableName: "result"})
+export class Submission extends Model<Submission> {
+  @Column({ primaryKey: true, autoIncrement: true })
+  idProb: number;
+
+  @Column
+  name: string;
+
+  @Column({ unique: true })
+  sname: string;
+
+  @Column
+  score: number;
+
+  @Column
+  time: number;
+
+  @Column
+  memory: number;
+
+  @Column({ defaultValue: 0})
+  state: number;
+
+  @Column({ defaultValue: 0})
+  lastOpen: number
+
+  @Column({ defaultValue: null})
+  case: string
+
+  @Column({ defaultValue: null})
+  rating: number
+}
