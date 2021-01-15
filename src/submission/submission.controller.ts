300c5a38a540ee546e7db62e7d77033ea7f2f573
Karnjj <karnjj@hotmail.com>
2021-01-15 20:13:58 +0700

    ✨add getAll getOne and getByidUser route for submission

--- src/submission/submission.controller.ts
+++ src/submission/submission.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SubmissionService } from './submission.service';
 
 @Controller('submission')
-export class SubmissionController {}
+export class SubmissionController {
+    constructor(private submissionService: SubmissionService) {}
+
+    @Get()
+    getAllSubmission() {
+        return this.submissionService.findAll()
+    }
+
+    @Get('/:idResult')
+    getSubmissionById(@Param('idResult') idResult: number) {
+        return this.submissionService.findOne({idResult})
+    }
+
+    @Get('/user/:idUser')
+    getAllSubmissionByIdUser(@Param('idUser') idUser: number) {
+        return this.submissionService.findAllByIdUser(idUser)
+    }
+}
--- src/submission/submission.service.ts
+++ src/submission/submission.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Submission } from './submission.model';
 
 @Injectable()
-export class SubmissionService {}
+export class SubmissionService {
+    constructor(
+        @InjectModel(Submission)
+        private submissionModel: typeof Submission
+    ) {}
+
+    findAll(): Promise<Submission[]> {
+        return this.submissionModel.findAll({
+            limit: 1000,
+            order: [
+                ['idResult', 'DESC']
+            ]
+        })
+    }
+
+    findOne(arg: any): Promise<Submission> {
+        return this.submissionModel.findOne({ where: arg})
+    }
+
+    findAllByIdUser(idUser: number): Promise<Submission[]> {
+        return this.submissionModel.findAll({
+            where : {idUser},
+            order: [
+                ['idResult', 'DESC']
+            ]
+        })
+    }
+}
