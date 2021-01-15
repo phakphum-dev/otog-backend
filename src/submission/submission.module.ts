c4dea00a65763ffbe2d84a59a6db1ef3353ebc03
Karnjj <karnjj@hotmail.com>
2021-01-15 20:14:40 +0700

    ✨add submission model to module

--- src/submission/submission.module.ts
+++ src/submission/submission.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { SubmissionController } from './submission.controller';
+import { Submission } from './submission.model';
 import { SubmissionService } from './submission.service';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Submission])],
   controllers: [SubmissionController],
   providers: [SubmissionService]
 })
