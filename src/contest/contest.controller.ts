3fdd139e3fc69c0b53e5a6d56917ebc5ab4a2fc0
Karnjj <karnjj@hotmail.com>
2021-01-24 14:45:59 +0700

    💄add semicolon

--- src/contest/contest.controller.ts
+++ src/contest/contest.controller.ts
@@ -1,33 +1,34 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ContestService } from './contest.service';
 
+@ApiTags('contest')
 @Controller('contest')
 export class ContestController {
   constructor(private contestService: ContestService) {}
 
   @Get()
   getAllContest() {
-    return this.contestService.findAll()
+    return this.contestService.findAll();
   }
 
   @Get('/now')
   getCurrentContest() {
-    return this.contestService.currentContest()
+    return this.contestService.currentContest();
   }
 
   @Get('/:contestId')
   getContestById(@Param('contestId') contestId: number) {
-    return this.contestService.findOne({ contestId })
+    return this.contestService.findOne({ contestId });
   }
 
   @Get('/:contestId/submission')
   getContestSubmissionById(@Param('contestId') contestId: number) {
-    return 'unfinish'
+    return 'unfinish';
   }
 
   @Get('/:contestId/scoreboard')
   getContestScoreBoardById(@Param('contestId') contestId: number) {
-    return 'unfinish'
+    return 'unfinish';
   }
-
 }
