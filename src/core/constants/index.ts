17ed4ba7cc333aea9120eca746f4e2d33916a20a
Karnjj <karnjj@hotmail.com>
2021-04-12 14:20:06 +0700

    ✨declare contestProblem repository

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -7,6 +7,7 @@ export const USER_REPOSITORY = 'USER_REPOSITORY';
 export const PROBLEM_REPOSITORY = 'PROBLEM_REPOSITORY';
 export const REFRESHTOKEN_REPOSITORY = 'REFRESHTOKEN_REPOSITORY';
 export const CONTEST_REPOSITORY = 'CONTEST_REPOSITORY';
+export const CONTESTPROBLEM_REPOSITORY = 'CONTESTPROBLEM_REPOSITORY';
 export const SUBMISSION_REPOSITORY = 'SUBMISSION_REPOSITORY';
 export const JWT_PRIVATE = fs.readFileSync('./private.key', 'utf8');
 export const JWT_PUBLIC = fs.readFileSync('./public.key', 'utf8');
--- src/modules/contest/contest.provider.ts
+++ src/modules/contest/contest.provider.ts
@@ -1,9 +1,17 @@
-import { CONTEST_REPOSITORY } from 'src/core/constants';
+import {
+  CONTESTPROBLEM_REPOSITORY,
+  CONTEST_REPOSITORY,
+} from 'src/core/constants';
 import { Contest } from 'src/entities/contest.entity';
+import { ContestProblem } from 'src/entities/contestProblem.entity';
 
 export const contestProvider = [
   {
     provide: CONTEST_REPOSITORY,
     useValue: Contest,
+  },
+  {
+    provide: CONTESTPROBLEM_REPOSITORY,
+    useValue: ContestProblem,
   },
 ];
