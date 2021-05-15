22e0d9d8b2bada030d272fbf2d274a3002a75f97
Karnjj <karnjj@hotmail.com>
2021-05-15 22:09:14 +0700

    ✨import AuthModule and UserModule

--- src/modules/problem/problem.module.ts
+++ src/modules/problem/problem.module.ts
@@ -3,9 +3,11 @@ import { ProblemController } from './problem.controller';
 import { ProblemService } from './problem.service';
 import { problemProvider } from './problem.provider';
 import { ContestModule } from '../contest/contest.module';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [ContestModule],
+  imports: [ContestModule, AuthModule, UserModule],
   controllers: [ProblemController],
   providers: [ProblemService, ...problemProvider],
   exports: [ProblemService],
